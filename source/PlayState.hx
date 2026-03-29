eac7b098a451630296c03a9fc52280a615f0d353
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-03-29 11:48:21 -0500

    dirt block in center

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,9 +4,14 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var block:Block = new Block('dirt');
+
 	override public function create()
 	{
 		super.create();
+
+		add(block);
+		block.screenCenter();
 	}
 
 	override public function update(elapsed:Float)
