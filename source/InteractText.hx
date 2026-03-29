c577a756ffb3143f2d8ba0c50ffb548f928746ac
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-03-29 12:00:13 -0500

    enabled field

--- source/InteractText.hx
+++ source/InteractText.hx
@@ -4,14 +4,18 @@ import flixel.text.FlxText;
 
 class InteractText extends FlxText
 {
+	public var enabled:Bool = true;
 	public var onClick:FlxSignal = new FlxSignal();
+
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
-		underline = FlxG.mouse.overlaps(this);
+		if (enabled)
+			underline = FlxG.mouse.overlaps(this);
 
-		if (FlxG.mouse.justPressed && underline)
-            onClick.dispatch();
+		if (enabled)
+			if (FlxG.mouse.justPressed && underline)
+				onClick.dispatch();
 	}
 }
