5af3c63671052700a8c45856e8ae93c73c587657
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-03-29 11:44:14 -0500

    onclick

--- source/Block.hx
+++ source/Block.hx
@@ -1,13 +1,26 @@
+import flixel.FlxG;
+import flixel.util.FlxSignal;
 import flixel.FlxSprite;
 
 class Block extends FlxSprite
 {
-    public var block:String;
+	public var block:String;
+
+	public var onClick:FlxSignal;
 
 	override public function new(block:String)
 	{
 		super();
 
-        this.block = block;
+		this.block = block;
+		onClick = new FlxSignal();
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(this))
+			onClick.dispatch();
 	}
 }
