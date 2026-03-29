72577c7cad010e4c26ef953125f95e0be515005f
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-03-29 11:51:21 -0500

    centerBlockClick func todo

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,18 +4,22 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
-	public var block:Block = new Block('dirt');
+	public var centerBlock:Block = new Block('dirt');
 
 	override public function create()
 	{
 		super.create();
 
-		add(block);
-		block.screenCenter();
+		add(centerBlock);
+		centerBlock.screenCenter();
+
+		centerBlock.onClick.add(centerBlockClick);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
 	}
+
+	public function centerBlockClick() {}
 }
